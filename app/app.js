fbe3985fcb7f9d9af2052a52cbee4f360d5a7e88
Javier Anselmi <javieranselmi@gmail.com>
2016-06-21 10:28:20 -0300

    Abstracted questtionaire into view

--- app/app.js
+++ app/app.js
@@ -1,16 +1,38 @@
-var app = angular.module('app', ['ui.bootstrap']);
-app.controller('mainCtrl',['$scope',function($scope){
+var app = angular.module('app', ['ui.bootstrap','ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: './app/template/home.html',
+            controller: 'homeCtrl'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
+
+app.controller('homeCtrl',['$scope',function($scope){
     
   $scope.greet = "Prueba WOT-C";
-  
   $scope.today = function() {
     $scope.dt = new Date();
   };
-    
   $scope.today();
-
   $scope.select = {
       options: ["pato","perro","elefante","gaviota"]
+  };
+    
+  $scope.validateQuestion = function(question) {
+      return question.isReadyToShow || false;
   };
     
   $scope.questionList =
@@ -63,8 +85,6 @@ app.controller('mainCtrl',['$scope',function($scope){
 
     
     console.log($scope);
-    
-    
     
 }])
 
