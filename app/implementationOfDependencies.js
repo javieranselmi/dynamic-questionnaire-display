902fee7d06f57bc56b52f0c42faf4e103a86ac5d
Javier Anselmi <javieranselmi@gmail.com>
2016-06-22 19:10:59 -0300

    Finished implementation of section

--- app/implementationOfDependencies.js
+++ app/implementationOfDependencies.js
@@ -11,8 +11,55 @@ function Section(id, title, questionList) {
     this.id = id;
     this.title = title;
     this.questionList = questionList;
-    this.updateQuestions = function(){
-        //Yet to implement
+
+    this.updateQuestionsDependencies = function(modifiedQuestion){
+        angular.forEach(this.getDependantQuestions(modifiedQuestion), function(question) {
+                question.updateIsDependencyReady(questionList, modifiedQuestion);
+        });
+    }
+    this.updateQuestionsVisibility   = function(modifiedQuestion){
+        var nextQuestions = this.getNextQuestions(modifiedQuestion);
+        angular.forEach(nextQuestions, function(qn) {
+            qn.updateVisibility(modifiedQuestion);
+        })
+    }
+    this.update = function(modifiedQuestion){
+        this.updateQuestionsDependencies(modifiedQuestion);
+        this.updateQuestionsVisibility(modifiedQuestion);
+    }
+    this.getNextQuestions = function(question) {
+        var nextQuestions = [];
+        angular.forEach(this.questionList, function(qn) {
+                if (qn.showIndex - 1 === question.showIndex) {
+                    nextQuestions.push(qn);
+                }
+        });
+        return nextQuestions;
+    }
+    this.getDependantQuestions = function(question) {
+        var dependantQuestions = [];
+        angular.forEach(this.questionList, function(qn) {
+                if (qn.dependsOn(question)) {
+                    dependantQuestions.push(qn);
+                }
+        });
+        return dependantQuestions;
+    } 
+    this.submitAnswerAndUpdate = function(questionId, answer) {
+        var qn = this.getQuestionById(questionId);
+        if (angular.isDefined(qn)) {
+            qn.submitAnswer(answer);
+            this.update(qn);
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    this.getQuestionById = function(questionId) {
+        return this.questionList.filter(function(qn) {
+            return qn.id === questionId;
+        })[0];
     }
 }
 
@@ -48,44 +95,66 @@ function Question(id, text, type, dependencies, showIndex, validation, selectVal
     
     //Properties with defaults
     this.isDependencyReady = false;
-    this.isShowIndexReady = false;
     this.isAnswerValid = false;
+    this.visibility = showIndex === 0? true : false;
     
-    //Methods
-    this.update = function(questionList, updatedQuestion) {
+    this.getDependenciesOfQuestion = function(question){
+        var dependenciesOfQuestion = [];
+        angular.forEach(this.dependencies, function(dep) {
+                if (dep.id === question.id) {
+                    dependenciesOfQuestion.push(dep);
+                }
+        });
+        return dependenciesOfQuestion;
+    }
+    this.updateIsDependencyReady = function(questionList, modifiedQuestion) {
+        this.updateDependencyFulfillment(questionList,modifiedQuestion);
+        //If all dependencies fulfilled change isDependencyReady
+        if(this.hasAllDependenciesFulfilled()) { 
+                        this.isDependencyReady = true; //Se cumplio la dependencia y todas las demás.
+        } else {
+                        this.isDependencyReady = false; //Se cumplio la dependencia pero aún no esta ready.
+        }
+    }
+    this.updateDependencyFulfillment = function(questionList, modifiedQuestion) {
 
         //Retrieve dependency to the updated question
-        var dependencyList = this.dependencies.filter( function(dependency) {
-            return dependency.id === updatedQuestion.id;
-        });
-
+        var dependencyList = this.getDependenciesOfQuestion(modifiedQuestion);
+        
         //If the updated question not within dependencies, returns false.
         if (dependencyList.length === 0) {
             return false;
         } else { //If the updated question within dependencies, update the dependency fulfilled propery.
             angular.forEach(dependencyList, function(dependency) {
-                dependency.updateFulfilled(updatedQuestion)
+                dependency.updateFulfilled(modifiedQuestion)
             });
         }
-        
-        //If all dependencies fulfilled change isDependencyReady
-        if(this.allDependenciesFulfilled()) { 
-                        this.isDependencyReady = true; //Se cumplio la dependencia y todas las demás.
-        } else {
-                        this.isDependencyReady = false; //Se cumplio la dependencia pero aún no esta ready.
-        }
+
     };
-    
-    this.allDependenciesFulfilled = function() {
+    this.hasAllDependenciesFulfilled = function() {
         return this.dependencies.every(function(dep){
-                        dep.fulfilled === true;
+                        return dep.fulfilled === true;
         })
     };
-    
-    this.validateAnswer = function () {
+    this.validateAnswer = function() {
         //
     };
-    
+    this.dependsOn = function(question) {
+        return this.dependencies.some(function(dep) {
+                return dep.id === question.id;
+        });
+    }
+    this.updateVisibility = function(modifiedQuestion) {
+        if (this.hasAllDependenciesFulfilled() && modifiedQuestion.hasBeenAnswered()) {
+            this.visibility = true;
+        }
+    }
+    this.hasBeenAnswered = function() {
+        return !(angular.isUndefined(this.answer) || this.answer === "");
+    }
+    this.submitAnswer = function(answer) {
+        this.answer = answer;
+    }
 }
     
 //######################################## DEPENDENCY ####################################
@@ -155,16 +224,17 @@ var v2 = new Validation({mustBeEqualTo: false});
 var d1 = new Dependency(1, v1);
 var d2 = new Dependency(1, v2);
 
-var q1 = new Question(1, "Have you ever done prison time for a federal fellony?", "bool", [], 1);
-var q2 = new Question(2, "How long where you in prison? (answer in days)"       , "text", [d1], 2);
-var q3 = new Question(3, "Were you ever ordered to do community work?"          , "bool", [d2], 2);
-var q4 = new Question(4, "When did you join the company?"                       , "date", [], 3);
-var q5 = new Question(5, "Do you live in an empowered zone?"                    , "bool", [], 4);
+var q1 = new Question(1, "Have you ever done prison time for a federal fellony?", "bool", [], 0);
+var q2 = new Question(2, "How long where you in prison? (answer in days)"       , "text", [d1], 1);
+var q3 = new Question(3, "Were you ever ordered to do community work?"          , "bool", [d2], 1);
+var q4 = new Question(4, "When did you join the company?"                       , "date", [], 2);
+var q5 = new Question(5, "Do you live in an empowered zone?"                    , "bool", [], 3);
 
 var questionList = [q1,q2,q3,q4,q5];
-    
 var section = new Section(1, "Personal Info", questionList);
-console.log(section);
-section.questionList[0].answer = true;
-section.questionList[1].update(section.questionList, section.questionList[0]);
-console.log(section);
+
+console.log( "Before answer changes", section);
+section.submitAnswerAndUpdate(1,false);
+console.log("After submitted answer 1", section);
+section.submitAnswerAndUpdate(3,true);
+console.log("After submitted answer 3", section);
